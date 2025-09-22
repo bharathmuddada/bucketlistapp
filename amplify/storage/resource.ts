759a537e71d83b79e39beed42db26db5a545dd2c
bharathmuddada <muddadabharath@gmail.com>
2025-09-22 13:24:21 -0700

    bucket list tracker app

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,4 +1,4 @@
-import { type ClientSchema, a, defineData,defineStorage } from '@aws-amplify/backend';
+import { defineStorage } from '@aws-amplify/backend';
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -6,24 +6,6 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any unauthenticated user can "create", "read", "update", 
 and "delete" any "Todo" records.
 =========================================================================*/
-const schema = a.schema({
-  BucketItem: a
-    .model({
-      title: a.string(),
-      description: a.string(),
-      image: a.string(),
-    })
-    .authorization((allow) => [allow.owner()]),  // Restrict access to the owner
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
-  },
-});
 
 export const storage = defineStorage({
   name: "amplifyBucketTrackerImages",
